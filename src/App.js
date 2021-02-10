b60e9d6b8aed5acf3f1fbef375bdb3df0af1f895
MTZGER <66328710+MTZGER@users.noreply.github.com>
2021-02-10 17:45:41 +0100

    next_change_two

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./unnamed/Home"
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
-      <h2>Test</h2>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
